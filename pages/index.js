75af850c29af3450619f40c51c3b7bdd53e2106a
Teppei Torii <teppeichan9@gmail.com>
2022-11-09 14:25:48 +0900

    Feat: ブログ記事(mdファイル)実装、blogページにpropsとして記事内容を渡す

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
-import h1Style from "../styles/index.module.css";
+import Link from "next/link";
+// import h1Style from "../styles/index.module.css";
 
 const Index = () => {
   return (
     <>
-      {/* Tailwind */}
       <h1 className="text-3xl font-bold underline">こんにちは</h1>
-      {/* module css */}
-      <h1 className={h1Style.h1_style}>こんにちは</h1>
+      <Link href={"/contact"}>Contactページへ移動</Link>
+      {/* module cssを使用する場合 */}
+      {/* <h1 className={h1Style.h1_style}>こんにちは</h1> */}
     </>
   )
 };
